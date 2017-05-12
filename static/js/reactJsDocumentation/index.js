8e436f1301e4d771ceb7998daea5669f5ee18154
aaxlss <axl9305@gmail.com>
2017-05-11 20:37:11 -0500

    using props

--- static/js/reactJsDocumentation/index.js
+++ static/js/reactJsDocumentation/index.js
@@ -3,14 +3,55 @@
  */
 
 //Component
-//===Jquery sintax===\\
+//===JSX sintax===\\
+var CommentList = React.createClass({
+    render: function () {
+        return (
+            <div className="commentList">
+                <Comment author="Pete Hunt">
+                    This is one comment
+                </Comment>
+                <Comment author="Jordan Walke">
+                    This is another comment
+                </Comment>
+            </div>
+        );
+    }
+});
+
+var CommentForm = React.createClass({
+    render: function () {
+        return (
+            <div className="commenForm">
+                CommentForm
+            </div>
+        );
+    }
+});
+
 var CommentBox = React.createClass({
     render: function () {
         return (
             <div className="commentBox">
-                Hello, world!
+
+                <h1>Comments</h1>
+                <CommentList/>
+                <CommentForm/>
             </div>
         );
+    }
+});
+
+var Comment = React.createClass({
+    render: function () {
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">
+                    {this.props.author}
+                </h2>
+                {this.props.children}
+            </div>
+        )
     }
 });
 // Rendering the component
